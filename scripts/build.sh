a3ecb755d01c171b50e3dbb16b4ed29afa9a4997
mengxk <xiaokun.meng@qcast.cn>
2019-01-22 09:48:11 +0800

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,22 +5,21 @@ set -o nounset
 
 CURRENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd);
 PROJECT_DIR=$(dirname "$CURRENT_DIR")
-DEPENDS_DIR="$PROJECT_DIR/config/scripts";
+DEPENDS_DIR="$PROJECT_DIR/config";
 
-if [ ! -d "$DEPENDS_DIR" ]; then
-	git submodule init;
-	git submodule update;
-fi
+cd "$PROJECT_DIR";
+git submodule init;
+git submodule update;
 
 build_extfunc_depends()
 {
-	"$DEPENDS_DIR/build-curl.sh" $@;
-	"$DEPENDS_DIR/build-sqlite.sh" $@;
+	"$DEPENDS_DIR/scripts/build-curl.sh" $@;
+	"$DEPENDS_DIR/scripts/build-sqlite.sh" $@;
 
 	#CFG_BUILD_ROOT
-	"$DEPENDS_DIR/build-Elastos.SDK.Keypair.C.sh" $@;
+	"$DEPENDS_DIR/scripts/build-Elastos.SDK.Keypair.C.sh" $@;
 	
 }
 export CFG_PROJECT_DIR="$PROJECT_DIR";
-source "$DEPENDS_DIR/build.sh" $@;
+source "$DEPENDS_DIR/scripts/build.sh" $@;
 
